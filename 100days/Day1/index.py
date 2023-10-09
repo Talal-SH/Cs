8e929c5c8e54436aae3c3caf11395b6097b58f27
Talal-PC <Talal.alshdoukhi@gmail.com>
2023-10-10 01:40:19 +0300

    i think i got the hang of changing then pushing .... ithink

--- 100days/Day1/index.py
+++ 100days/Day1/index.py
@@ -1,15 +1,22 @@
 # this is name generator
-wellcome = ('wellcome to the name username Generator')
+wellcome = ('Wellcome to the Username Generator. \nA name will be generated based on the answers')
 print(wellcome)
-name = input("Give me your First name  ") 
-date = input("Give me year of birth ") 
-color = input("Give me a Color ")
+name = input("Give me your First name: ")
+while True:
+    try: 
+        date = int(input('Give me the Year of birth: '))
+        dateconfirmed = date
+        break
+    except ValueError:
+        print('Please provide the Year of birth in integers, ex. 1999 or 99: ')
+
+color = input("Give me a Color: ")
 
 
-result_date = int(date) % 100
+result_date = dateconfirmed % 100
 
 
 result = f'{color[0] }{name.capitalize()}{result_date}{color[-1]}'
 
 
-print(f'Your generated name is: {result}')
+print(f'Your Generated Username is: {result}')
